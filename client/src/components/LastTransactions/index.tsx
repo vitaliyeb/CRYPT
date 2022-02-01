4f0d74ef51f35ccb39ffdafbc3c2045dd3fbed76
Vitaliy Burdin <vlvlv.vovov@bk.ru>
2022-02-01 12:55:33 +0500

    latest transaction component

---
+++ client/src/components/LastTransactions/index.tsx
@@ -0,0 +1,10 @@
+import styles from './style.module.css';
+
+const LastTransactions = () => {
+
+    return (<div className={styles.wrapper}>
+        <p>Connect your account to see the latest transactions</p>
+    </div>)
+}
+
+export default LastTransactions;
--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header";
 import React from "react";
 import SendCrypto from "../../components/SendCrypto";
 import Services from "../../components/Services";
+import LastTransactions from "../../components/LastTransactions";
 
 
 
@@ -23,6 +24,7 @@ const Main = () => {
         <div className={styles.defaultBg}>
             <Layout>
                 <Services />
+                <LastTransactions />
             </Layout>
         </div>
 
